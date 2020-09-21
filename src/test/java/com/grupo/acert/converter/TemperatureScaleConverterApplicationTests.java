8db9b9ce374c1b06efe8c6633d5848b56b5e3c7c
Alex de Freitas <alex_freitas@sicredi.com.br>
2020-09-21 15:11:44 -0300

    First  commit

---
+++ src/test/java/com/grupo/acert/converter/TemperatureScaleConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo.acert.converter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemperatureScaleConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
