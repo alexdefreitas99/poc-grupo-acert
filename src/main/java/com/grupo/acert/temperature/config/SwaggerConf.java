38d010f5b0d442ce39003d9653821814b11ebf05
Alex de Freitas <alex_freitas@sicredi.com.br>
2020-09-22 08:42:47 -0300

    Fix swagger and db

--- src/main/java/com/grupo/acert/temperature/config/SwaggerConf.java
+++ src/main/java/com/grupo/acert/temperature/config/SwaggerConf.java
@@ -8,7 +8,6 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 public class SwaggerConf {
@@ -18,7 +17,7 @@ public class SwaggerConf {
         var metadata = metaData();
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.grupo.acert"))
+                .apis(RequestHandlerSelectors.basePackage("com.grupo.acert.temperature"))
                 .build()
                 .apiInfo(metadata)
                 .enableUrlTemplating(true);
